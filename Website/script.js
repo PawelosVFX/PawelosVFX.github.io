c5011acc62a088558a4a8a0c61e6f2f4234e9982
Pawel Tryzna <Pawel Tryzna>
2016-01-13 23:51:51 +0100

    added script.js

--- Website/script.js
+++ Website/script.js
@@ -1,28 +1,37 @@
 $(document).ready(function() {
     $("button.texta").click(function(){
-            $("button.texta").fadeOut(350);
-            $("button.textb").fadeOut(350);
-            $("button.textc").fadeOut(350);
+            $("button.texta").fadeOut(500);
+            $('button.texta').attr("disabled", true);  
+            $("button.textb").fadeOut(500);
+            $("button.textc").fadeOut(500);
                 setTimeout(function(){
-            $("img.imga").fadeIn();}, 350);
+            $("img.imga").fadeIn();}, 600);
                 setTimeout(function(){
-            $("img.imgb").fadeIn();}, 350);
+            $("img.imgb").fadeIn();}, 600);
                 setTimeout(function(){
-            $("button.return").fadeIn();}, 350);
+            $('.return').attr("disabled", false); 
+            $(".return").fadeIn();}, 600);
                 setTimeout(function(){
-            $("p").fadeIn();}, 350);
+            $("p").fadeIn();}, 600);
+                setTimeout(function(){
+            $(".box").fadeIn();}, 600);
 
     });
       $("button.return").click(function(){
             setTimeout(function(){
-        $("button.texta").fadeIn();}, 350);
+        $('button.texta').attr("disabled", false)
+        $("button.texta").fadeIn();}, 650);
             setTimeout(function(){
-        $("button.textb").fadeIn();}, 350);
+        $("button.textb").fadeIn();}, 650);
             setTimeout(function(){
-        $("button.textc").fadeIn();}, 350);
-        $("img.imga").fadeOut(350);
-        $("img.imgb").fadeOut(350);
-        $("button.return").fadeOut(350);
-        $("p").fadeOut(350);
+        $("button.textc").fadeIn();}, 600);
+        $("img.imga").fadeOut(500);
+        $("img.imgb").fadeOut(500);
+        $(".return").fadeOut(500);
+        $('.return').attr("disabled", true)
+        $("p").fadeOut(500); 
+        $(".box").fadeOut(500); 
+
 });
 });
+
